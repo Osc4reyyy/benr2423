b19c5a629ea48b656a372b73f4b64a120277e891
Alief Irfan <giftforoscar@gmail.com>
2026-01-03 16:48:50 +0800

    updated message

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const port = process.env.PORT || 8080;
 app.use(express.json())
 
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    res.send('Hello World! This is Alief deployment.')
 })
 
 app.listen(port, () => {
